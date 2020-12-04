25e2e506a4b4cb0b34c372d6f296a98dc4a95a3c
Daniel <63892732+Galactius@users.noreply.github.com>
2020-12-04 17:54:57 -0600

    Added more comments, input explanation, and added remaining 12 major and 15 minor scales to switch statement.

--- BasicMelodyGenerator.java
+++ BasicMelodyGenerator.java
@@ -1,5 +1,5 @@
 //Author: Daniel Perez
-//Date: 11/29/20
+//Date: 12/4/20
 //Objective: Generate a basic melody using random notes within a selected scale, with user-defined length. 
 import java.util.Scanner; 
 import java.util.Random;  
@@ -9,6 +9,8 @@ public class BasicMelodyGenerator
    public static void main(String[] args) 
    {        
       System.out.println("---------------------Basic Melody Generator---------------------"); 
+      System.out.println("Note: # is sharp, f is flat.");
+      System.out.println("Example scales: C Major, df major, cminor...");
 
       //prompt user for number of notes to generate
       Scanner in = new Scanner(System.in);
@@ -36,15 +38,22 @@ public class BasicMelodyGenerator
       //statement and returns
       String[] notes = scaleNotes(input).split(",");
          
-      System.out.print("Generating random notes... ");
+      System.out.println("Generating random notes... ");
 
       //for loop allows program to generate multiple notes or a singular note
+      System.out.print("Generated Melody: ");
       for(int i = 0; i < numNotes; i++)
       {
          System.out.print(randNote(notes) + " ");
       }
 
       in.close();
+
+      //General program flow:
+      //IN: numNotes, input (this is the name of the entered scale)
+      //numNotes (info stays in main) -> output
+      //input -> scaleNotes -> (converted into string containing notes in scale) main ->
+      // (converted into string array) randNotes -> output
    } 
 
    //randNotes takes in string array, and returns a random index within the passed in array
@@ -66,23 +75,115 @@ public class BasicMelodyGenerator
 
       //System.out.println("strLwr: "+strLwr+", fixedScale: "+fixedScale);
 
-      String resp;
+      String resp; //resp is a placeholder variable, used only for the switch and return
       switch(fixedScale)
       {
-         case "c":
-         resp =  "C,C#,D,D#,E,F,F#,G,G#,A,A#";
+         case "cmajor":
+            resp = "C,D,E,F,G,A";
          break;
 
-         case "cmajor":
-         resp = "C,D,E,F,G,A";
+         case "gmajor":
+            resp = "G,A,B,C,D,E,F#";
+         break;
+
+         case "dmajor":
+            resp = "D,E,F#,G,A,B,C#";
+         break;
+
+         case "amajor":
+            resp = "A,B,C#,D,E,F#,G#";
+         break;
+
+         case "emajor":
+            resp = "E,F#,G#,A,B,C#,D#";
+         break;
+
+         case "bmajor":
+            resp = "B,C#,D#,E,F#,A#";
+         break;
+
+         case "f#major":
+            resp = "F#,G#,A#,B,C#,D#,E#";
+         break;
+
+         case "dfmajor":
+            resp = "Df,Ef,F,Gf,Af,Bf,C";
+         break;
+
+         case "afmajor":
+            resp = "Af,Bf,C,Df,Ef,F,G";
+         break;
+
+         case "bfmajor":
+            resp = "Bf,C,D,Ef,F,G,A";
+         break;
+
+         case "fmajor":
+            resp = "F,G,A,Bf,C,D,E";
+         break;
+
+         case "aminor":
+            resp = "A,B,C,E,F,G,A";
+         break;
+
+         case "dminor":
+            resp = "D,E,F,G,A,Bf,C,D";
+         break;
+
+         case "gminor":
+            resp = "G,A,Bf,C,D,Ef,F,G";
          break;
 
          case "cminor":
-         resp = "C,D,D#,F,G,G#,A#,C";
+            resp = "C,D,Ef,F,G,Af,Bf,C";
+         break;
+
+         case "fminor":
+            resp = "F,G,Af,Bf,C,Df,Ef,F";
+         break;
+
+         case "afminor":
+            resp = "Af,Bf,Cf,Df,Ef,Gf,Af";
+         break;
+
+         case "efminor":
+            resp = "Ef,F,Gf,Af,Bf,Cf,Df,Ef";
+         break;
+
+         case "bfminor":
+            resp = "Bf,C,Df,Ef,F,Gf,Af,Bf";
+         break;
+
+         case "eminor":
+            resp = "E,F#,G,A,B,C,D,E";
+         break;
+
+         case "bminor":
+            resp = "B,C#,D,E,F#,G,A,B";
+         break;
+
+         case "f#minor":
+            resp = "F#,G#,A,B,C#,D,E,F#";
+         break;
+
+         case "c#minor":
+            resp = "C#,D#,E,F#,G#,A,B,C#";
+         break;
+
+         case "g#minor":
+            resp = "G#,A#,B,C#,D#,E,F#,G#";
+         break;
+
+         case "d#minor":
+            resp = "D#,E#,F#,G#,A#,B,C#,D#";
+         break;
+
+         case "a#minor":
+            resp = "A#,B#,C#,D#,E#,F#,G#,A#";
          break;
 
          default:
-         resp = "Scale not available";
+            resp = "Scale not available";
          break;
       }
 
